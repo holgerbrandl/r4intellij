075506cd676f5414ca3698e590cbad94b73231bc
Holger Brandl <brandl@mpi-cbg.de>
2011-12-13 01:28:00 +0100

    first live template

---
+++ src/com/r4intellij/editor/LiveTemplateProvider.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2011 Holger Brandl
+ *
+ * This code is licensed under BSD. For details see
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
+
+package com.r4intellij.editor;
+
+import com.intellij.codeInsight.template.impl.DefaultLiveTemplatesProvider;
+import org.jetbrains.annotations.NonNls;
+
+
+/**
+ * Add some live templates for R
+ *
+ * @author Holger Brandl
+ */
+public class LiveTemplateProvider implements DefaultLiveTemplatesProvider {
+
+    private static final
+    @NonNls
+    String[] DEFAULT_TEMPLATES = new String[]{
+            "/liveTemplates/rtemplates"
+//            "/liveTemplates/surround"
+    };
+
+    public String[] getDefaultLiveTemplateFiles() {
+        return DEFAULT_TEMPLATES;
+    }
+
+    @Override
+    public String[] getHiddenLiveTemplateFiles() {
+        return null;
+    }
+}
