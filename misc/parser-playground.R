4abcc7899d9e51fbca018e920309dfd8b3a30e24
Holger Brandl <brandl@mpi-cbg.de>
2011-12-22 17:31:29 +0100

    started antlr-based grammar implementation

--- misc/parser-playground.R
+++ misc/parser-playground.R
@@ -6,3 +6,7 @@ setwd("/Users/brandl/projects/rplugin/r4intellij")
 require( parser )
 p <- parser( "./misc/parser-example.R" )
 p
+
+
+pp <- parser("/Users/brandl/projects/rplugin/parser-package/inst/example/f.R")
+pp
